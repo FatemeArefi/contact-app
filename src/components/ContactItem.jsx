fddf10d18856a1292b610b5769a5cfa80c7cbdb3
FatemeArefi <f.arefi982@gmail.com>
2024-09-22 00:40:12 +0330

    Add files via upload

---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,23 @@
+function ContactItem({
+  data: { id, name, lastName, email, phone },
+  deleteHandler,
+}) {
+  return (
+    <li key={id}>
+      <p>
+        {name}
+        {lastName}
+      </p>
+      <p>
+        <span>📩</span> {email}
+      </p>
+      <p>
+        <span>📱</span>
+        {phone}
+      </p>
+      <button onClick={() => deleteHandler(id)}> 🗑 </button>
+    </li>
+  );
+}
+
+export default ContactItem;
